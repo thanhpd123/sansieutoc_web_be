84a6a3895892eda181f80df8d1c499de6a7bf923
thanhpd123 <thanhpdhe173100@fpt.edu.vn>
2025-06-17 22:13:17 +0700

    change cors sansieutoc.com

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 // ===== Middleware =====
 // CORS: Cho phép frontend truy cập (thay đổi origin nếu cần thiết)
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://sansieutoc-web-fe.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://sansieutoc-web-fe.vercel.app', 'https://www.sansieutoc.com', 'https://sansieutoc.com'],
   credentials: true,
 }));
 
