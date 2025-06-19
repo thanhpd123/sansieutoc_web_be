ad1bfcc0f97501d28ce9255d79504386170e9809
thanhpd123 <thanhpdhe173100@fpt.edu.vn>
2025-06-19 16:15:48 +0700

    fix lỗi token

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 // ===== Middleware =====
 // CORS: Cho phép frontend truy cập (thay đổi origin nếu cần thiết)
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://sansieutoc-web-fe.vercel.app', 'https://www.sansieutoc.com', 'https://sansieutoc.com', 'https://zkoo0400gsgowowok84o8cck.qroma.tinkering.vn'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://sansieutoc-web-fe.vercel.app', 'https://www.sansieutoc.com', 'https://sansieutoc.com'],
   credentials: true,
 }));
 
